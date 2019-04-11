472d8e97783c4cfc63d6a9052dfe523b0c7f03c8
Williams Ohworuka <47520577+WilliamsOhworuka@users.noreply.github.com>
2019-04-11 06:47:15 +0100

    feature(sign in): initail commit [start #165270833]

--- Backend/routes/auth.route.js
+++ Backend/routes/auth.route.js
@@ -5,5 +5,5 @@ import User from '../controllers/signup.controller';
 const router = express.Router();
 
 router.post('/signup', Validate.checkEmptyFields, Validate.checkValidInput, User.create);
-
+router.post('/signin', (req,res,next) => {});
 export default router;
