e826db55d4ebf19d185226578410f24cdbe384b7
Williams Ohworuka <47520577+WilliamsOhworuka@users.noreply.github.com>
2019-04-26 06:53:56 +0100

    feature(create user): create endpoint to enable admin create users [finishes #165628530]

--- Backend/routes/auth.route.js
+++ Backend/routes/auth.route.js
@@ -2,9 +2,11 @@ import express from 'express';
 import SigninMiddleware from '../middleware/signin.middleware';
 import Auth from '../controllers/authentication.controller';
 import SignupMiddleware from '../middleware/signup.middleware';
+import AccountMiddleware from '../middleware/account.middleware';
 
 const router = express.Router();
 
 router.post('/signup', SignupMiddleware.checkEmptyFields, SignupMiddleware.checkValidInput, SignupMiddleware.exists, Auth.createUser);
 router.post('/signin', SigninMiddleware.checkEmptyFields, SigninMiddleware.checkExistence, Auth.sendResponse);
+router.post('/admin/create', SignupMiddleware.checkEmptyFields, SignupMiddleware.checkEmptyFields, SignupMiddleware.exists, AccountMiddleware.checkAuthorization, Auth.AdmincreateUser);
 export default router;
