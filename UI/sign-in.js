d2176e0ea183e761f88230ccf3cbacb0b3516be6
Williams Ohworuka <47520577+WilliamsOhworuka@users.noreply.github.com>
2019-06-20 14:58:25 +0100

    feature(authentiaction): connect sign in page with api endpoint [finishes #166818858]

--- UI/sign-in.js
+++ UI/sign-in.js
@@ -1,10 +1,22 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
-const formData = new FormData(document.getElementById('signin-form'));
+const signinForm = document.getElementById('signin-form');
+const closeIcon = document.getElementById('close-error');
+const errorDiv = document.getElementById('auth-error');
 const submit = document.getElementById('signin-submit');
 
-submit.addEventListener('click', async (event) => {
-  event.preventDefault();
+const decodeToken = (token) => {
+  const base64Url = token.split('.')[1];
+  return JSON.parse(window.atob(base64Url));
+};
+submit.addEventListener('click', () => {
+  submit.style.backgroundColor = '#9b3082';
+  submit.value = 'Signing in...';
+});
+
+signinForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(signinForm);
   const searchParams = new URLSearchParams();
 
   const pair = [...formData];
@@ -12,11 +24,32 @@ submit.addEventListener('click', async (event) => {
     searchParams.append(pair[i][0], pair[i][1]);
   }
 
-  const response = await fetch('https://infinite-sea-96838.herokuapp.com/api/v1/auth/signin', {
-    method: 'POST',
+  fetch('https://infinite-sea-96838.herokuapp.com/api/v1/auth/signin', {
+    method: 'post',
     body: searchParams,
-  });
+  }).then(res => res.json())
+    .then((res) => {
+      console.log(res);
+      if (!res.error) {
+        const userData = decodeToken(res.data.token);
+        const userType = userData.type;
+        const { isAdmin } = userData;
 
-  const result = await response.json();
-  console.log(result.message);
+        localStorage.setItem('user-data', JSON.stringify(res.data));
+        if (userType === 'staff') {
+          location.href = isAdmin ? 'https://williamsohworuka.github.io/Banka-web-app/UI/admin-dashboard.html'
+            : 'https://williamsohworuka.github.io/Banka-web-app/UI/staff-accountDashboard.html';
+        } else {
+          location.href = 'https://williamsohworuka.github.io/Banka-web-app/UI/account-dashboard.html';
+        }
+      } else {
+        submit.value = 'Sign in';
+        submit.style.backgroundColor = '#660066';
+        errorDiv.style.display = 'block';
+      }
+    });
+});
+
+closeIcon.addEventListener('click', () => {
+  errorDiv.style.display = 'none';
 });
